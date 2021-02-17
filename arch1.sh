43aaea055821806676162f26fa825d339021335b
tat <t@t>
2021-02-17 08:28:46 +0300

    'asdfasdf'

--- arch1.sh
+++ arch1.sh
@@ -56,4 +56,4 @@ pacstrap /mnt base base-devel linux linux-firmware vim dhcpcd netctl
 echo '3.3 Настройка системы'
 genfstab -pU /mnt >> /mnt/etc/fstab
 
-arch-chroot /mnt sh -c "$(curl -fsSL git.io/arch2.sh)"
+arch-chroot /mnt sh -c "$(curl -fL git.io/arch2.sh)"
