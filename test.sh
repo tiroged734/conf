bca5086a65e34a83126806f9e78cf254804cb7fb
tat <t@t>
2021-02-17 07:25:37 +0300

    'hhh'

--- test.sh
+++ test.sh
@@ -1,6 +1,14 @@
+loadkeys ru
+setfont cyr-sun16
+echo 'Скрипт сделан на основе чеклиста Бойко Алексея по Установке ArchLinux'
+echo 'Ссылка на чек лист есть в группе vk.com/arch4u'
+
+echo '2.3 Синхронизация системных часов'
+timedatectl set-ntp true
+
 echo '2.4 создание разделов'
 (
-  fdisk /dev/sda
+  fdisk /dev/sda;
   echo o;
 
   echo n;
