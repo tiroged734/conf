6873ca638eeeede7bd5049d58609314a7a3ede10
tat <t@t>
2021-02-18 12:00:00 +0300

    'asdfasdf'

--- arch3.sh
+++ arch3.sh
@@ -1,18 +1,17 @@
-su -u nobody wget git.io/yay-install.sh
+wget git.io/yay-install.sh
 
-su -u nobody sh yay-install.sh --noconfirm)
+sh yay-install.sh --noconfirm
 
-su -u nobody yay -Syy)
-su -u nobody yay -S polybar --noconfirm)
+yay -Syy
+yay -S polybar --noconfirm
 
-su -u nobody git clone https://github.com/tiroged734/conf.git)
+git clone https://github.com/tiroged734/conf.git
 
-su -u nobody rm -r $HOME/.config/*)
-(su -u nobody
+rm -r $HOME/.config/*
+
 for file in ./conf/dotfiles/*
 do
-tempfile="/home/$username/.config/${BASH_REMATCH[1]}"
+tempfile="$HOME/.config/${BASH_REMATCH[1]}"
 ln -s "$file" "$tempfile"
 ln -s "$tempfile" "${tempfile%.*}"
 done
-)
